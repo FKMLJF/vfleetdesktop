081162a9cd82614d66e22f90df275e1bc164f84f
vincze_jozsef <vincze.jozsef.develop@gmail.com>
2020-05-14 21:52:59 +0200

    ertesitesek

--- resources/views/mail.blade.php
+++ resources/views/mail.blade.php
@@ -49,7 +49,7 @@
     @yield('extra_header')
 </head>
 <body style="background-image: radial-gradient(circle, #ffffff, #f8f8fd, #f1f2fc, #e7ecfa, #dde7f9);">
-<div class="container bg-secondary" style="background: rgba(165,199,255,0.36)">
+<div class="container bg-secondary" style="background: rgb(180,210,255); padding: 20px">
     <h1 style="background: #0b51c5; color: white; padding: 50px; font-size: 32px">{{$data['title']}} ({{$data['auto']}})</h1>
 
     <p style="padding: 20px; color: #00040a; font-size: 22px">Értesítés: <strong class="text-primary">{{$data['ertesites']}}</strong></p>
